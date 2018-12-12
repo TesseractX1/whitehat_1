a7134fbb2932d71ebb34d7f9932320926fae4dfd
Polar Shock <f.postnov@gmail.com>
2018-12-12 20:25:46 +0800

    GUIstuff

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
+	public int cpu = 0;
+	public int ram = 0;
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
---
+++ Assets/Whitehat/PlayerScript.cs
@@ -0,0 +1,20 @@
+namespace Whitehat.Player{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	public class PlayerScript : MonoBehaviour {
+
+		public int cpu = 0;
+		public int ram = 0;
+		// Use this for initialization
+		void Start () {
+
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+		}
+	}
+}
