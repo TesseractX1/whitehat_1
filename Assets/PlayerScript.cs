b0af8ed65932cedddce6f27cc24636b95c877cbc
Polar Shock <f.postnov@gmail.com>
2018-12-31 15:11:34 +0500

    ui + player

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour {
-	public int cpuNum;
-	public GameObject ramNum;
+	//public int cpuNum;
+	//public GameObject ramNum;
+	public Text cpucounter;
+	public Text ramcounter;
 	public int cpu = 0;
 	public int ram = 0;
-	// Use this for initialization
-	void Start () {
-
-	}
-
 	// Update is called once per frame
 	void Update () {
-
+		cpucounter.text = cpu.ToString();
+		ramcounter.text = ram.ToString();
 	}
 }
